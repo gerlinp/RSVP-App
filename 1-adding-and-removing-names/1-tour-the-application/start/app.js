96d204f4215cb60072feeedc26681958f4563bb5
gerlinp <gerlin.pierrelouis@gmail.com>
2021-03-11 01:03:32 -0500

    Refactoring pt 1

--- 1-adding-and-removing-names/1-tour-the-application/start/app.js
+++ 1-adding-and-removing-names/1-tour-the-application/start/app.js
@@ -36,22 +36,23 @@ filterCheckBox.addEventListener('change', (e) => {
 });
 
 function createLI(text) {
+  function createElement(elementName, property, value) {
+    const element = document.createElement(elementName);
+    element[property] = value;
+    return element;
+  }
+  function appendToLI(elementName, property, value) {
+    const element = createElement(elementName, property, value);
+    li.appendChild(element);
+    return element;
+  }
   const li = document.createElement('li');
-  const span = document.createElement('span');
-  span.textContent = text;
-  li.appendChild(span);
-  const label = document.createElement('label');
-  label.textContent = 'Confirmed';
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  label.appendChild(checkbox);
-  li.appendChild(label); 
-  const editButton = document.createElement('button');
-  editButton.textContent = 'edit';
-  li.appendChild(editButton); 
-  const removeButton = document.createElement('button');
-  removeButton.textContent = 'remove';
-  li.appendChild(removeButton);
+  appendToLI('span', 'textContent', text);
+  appendToLI('label', 'textContent', 'Confirmed')
+    .appendChild(createElement('input', 'type', 'checkbox'));
+  appendToLI('button', 'textContent', 'edit');
+  appendToLI('button', 'textContent', 'remove');
+
   return li;
 }
 
